faf54ea81d5e3849fbcee176cab1eef136e9b965
Sid9699 <ranasiddharth6@gmail.com>
2020-08-25 18:29:02 +0530

    Movie Search App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SearchMovie from "./components/SearchMovie";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovie />
+    </div>
+  );
+}
+
+export default App;
